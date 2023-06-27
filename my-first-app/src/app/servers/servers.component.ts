5197b8f2880efa303043458f38db06fdeab79839
Richard Calf <richard.calf@gmail.com>
2023-06-27 15:03:24 +0200

    section 2, video 27 property binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent {
+  allowNewServer = false;
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 5000);
+  }
 
 }
