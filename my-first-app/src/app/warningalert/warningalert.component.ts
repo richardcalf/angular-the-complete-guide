16cd0588bfc59f9e3dc81006521ac4fad7cffaac
Richard Calf <richard.calf@gmail.com>
2023-06-27 12:17:32 +0200

    assignment 1 : upgraded

--- my-first-app/src/app/warningalert/warningalert.component.ts
+++ my-first-app/src/app/warningalert/warningalert.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-warning',
-  templateUrl: './warningalert.component.html'
+  templateUrl: './warningalert.component.html',
+  styleUrls: ['./warningalert.component.css']
   })
 export class WarningAlertComponent {
 
