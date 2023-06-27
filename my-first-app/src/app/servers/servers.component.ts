c94c18c1ce9477ec75293852855ab8593f7eb6f4
Richard Calf <richard.calf@gmail.com>
2023-06-27 16:07:25 +0200

    section 2, video 31  object to fetch event data

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -8,6 +8,7 @@ import { Component } from '@angular/core';
 export class ServersComponent {
   allowNewServer = false;
   serverCreationStatus = "No servers exist.";
+  serverName = '';
 
   constructor() {
     setTimeout(() => {
@@ -17,5 +18,9 @@ export class ServersComponent {
 
   onCreateServer() {
     this.serverCreationStatus = 'Server was created.';
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
