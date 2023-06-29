6ebbb4791914bc2c5ca1f01ffe0644860e908b57
Richard Calf <richard.calf@gmail.com>
2023-06-29 11:13:35 +0200

    assignment 3 : directives (work in progress)

--- my-first-app/src/app/exercise-directives/exercise-directives.component.ts
+++ my-first-app/src/app/exercise-directives/exercise-directives.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./exercise-directives.component.css']
 })
 export class ExerciseDirectivesComponent {
+  displayParagraph = false;
+  buttonClicks = 0;
+  clicksArray = [];
 
+  onToggleDisplay() {
+    this.displayParagraph = !(this.displayParagraph);
+    this.buttonClicks++;
+    this.clicksArray.push(this.buttonClicks);
+  }
 }
