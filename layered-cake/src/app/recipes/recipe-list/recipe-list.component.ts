b0a22ba87d456aea68b7d7fb3e4710729e4c014f
Richard Calf <richard.calf@gmail.com>
2023-06-30 16:22:56 +0200

    section 3, video 53 adding content to the recipes component

--- layered-cake/src/app/recipes/recipe-list/recipe-list.component.ts
+++ layered-cake/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('Default Recipe','Enjoy the Goulash Stew',
+    'https://www.allrecipes.com/thmb/Q9CsPT8f6h5UydohsA5VILBFlrQ=/1500x750/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/2400-240708-broccoli-and-chicken-stir-fry-3x4-186-b7f290a400134ae9910f2e67ff50d9f2.jpg')
+  ];
 }
