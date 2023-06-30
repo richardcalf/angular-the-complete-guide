b070e29794b012c694a9eff7df7a1b296f660729
Richard Calf <richard.calf@gmail.com>
2023-06-30 15:00:05 +0200

    section 3, video 49 (instructor added components)

--- layered-cake/src/app/app.module.ts
+++ layered-cake/src/app/app.module.ts
@@ -2,22 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ShoppinglistComponent } from './shoppinglist/shoppinglist.component';
-import { ShoppinglistmodifierComponent } from './shoppinglist/shoppinglistmodifier/shoppinglistmodifier.component';
-import { RecipelistComponent } from './recipelist/recipelist.component';
-import { RecipelistitemComponent } from './recipelist/recipelistitem/recipelistitem.component';
-import { RecipelistdetailComponent } from './recipelist/recipelistdetail/recipelistdetail.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ShoppinglistComponent,
-    ShoppinglistmodifierComponent,
-    RecipelistComponent,
-    RecipelistitemComponent,
-    RecipelistdetailComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule
