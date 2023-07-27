8856e56887b14da0acd527dd89a8a5e575f802e1
Richard Calf <richard.calf@gmail.com>
2023-07-27 22:45:13 +0200

    section 6, video 85, adding navigation with event binding

--- layered-cake/src/app/header/header.component.ts
+++ layered-cake/src/app/header/header.component.ts
@@ -1,9 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, Output, EventEmitter } from "@angular/core";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    @Output() ribbonMenuClicked = new EventEmitter<number>();
+
+    onRibbonMenuClicked(menu: number) {
+        this.ribbonMenuClicked.emit(menu);
+    }
 
 }
