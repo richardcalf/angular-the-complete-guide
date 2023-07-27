7cd1ba1d73d63f7e2e18b4972036a0c7ce4ea508
Richard Calf <richard.calf@gmail.com>
2023-07-27 22:02:12 +0200

    menu clicks to display correct components

--- layered-cake/src/app/app.component.ts
+++ layered-cake/src/app/app.component.ts
@@ -6,4 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+   menuNumber: number = -1;
+
+  onShoppingClickEvent(number) {
+    console.log(number);
+    this.menuNumber = number;
+  }
+
+  onRecipesClickEvent(number) {
+    console.log(number);
+    this.menuNumber = number;
+  }
 }
--- layered-cake/src/app/header/header.component.ts
+++ layered-cake/src/app/header/header.component.ts
@@ -1,9 +1,19 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+    @Output() shoppingClickEvent = new EventEmitter<number>();
+    @Output() recipesClickEvent = new EventEmitter<number>();
+
+    onShoppingClick() {
+        this.shoppingClickEvent.emit(1);
+    }
+
+    onRecipesClick() {
+        this.recipesClickEvent.emit(0);
+    }
 
 }
