c2d7d116fe5006b478b417b260414fc8eb73b1bc
Richard Calf <richard.calf@gmail.com>
2023-08-02 14:06:52 +0200

    section 6, video 87, instructor lead solution

--- layered-cake/src/app/recipes/recipe-list/recipe-list.component.ts
+++ layered-cake/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,15 +7,14 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+  @Output() recipeListItemClicked = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('Goulash Recipe','Enjoy the Goulash Stew',
     'https://www.allrecipes.com/thmb/Q9CsPT8f6h5UydohsA5VILBFlrQ=/1500x750/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/2400-240708-broccoli-and-chicken-stir-fry-3x4-186-b7f290a400134ae9910f2e67ff50d9f2.jpg'),
     new Recipe('Chorizo Bake Recipe','Enjoy the Chorizo Bake',
     'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/gnocchi-1d16725.jpg')
   ];
-  @Output() recipeListItemClicked = new EventEmitter<Recipe>();
-
-
+  
   onRecipeItemClicked(recipe: Recipe) {
     console.log('onRecipeItemClicked '+recipe.name);
     this.recipeListItemClicked.emit(recipe);
