e821246a5e014bc9c9d229e02c4d9651e934ec66
Richard Calf <richard.calf@gmail.com>
2023-08-08 20:45:48 +0200

    section 10, video 118, managing recipes with a recipe service

--- layered-cake/src/app/recipes/recipe-list/recipe-list.component.ts
+++ layered-cake/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,22 +1,23 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+  constructor(private recipeService: RecipeService) {}
+
   @Output() recipeListItemClicked = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('Goulash Recipe','Enjoy the Goulash Stew',
-    'https://www.allrecipes.com/thmb/Q9CsPT8f6h5UydohsA5VILBFlrQ=/1500x750/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/2400-240708-broccoli-and-chicken-stir-fry-3x4-186-b7f290a400134ae9910f2e67ff50d9f2.jpg'),
-    new Recipe('Chorizo Bake Recipe','Enjoy the Chorizo Bake',
-    'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/gnocchi-1d16725.jpg')
-  ];
+  recipes: Recipe[];
+
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
+  }
   
   onRecipeItemClicked(recipe: Recipe) {
-    console.log('onRecipeItemClicked '+recipe.name);
     this.recipeListItemClicked.emit(recipe);
   }  
 }
