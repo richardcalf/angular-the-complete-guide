d97d4d0c2d20b0275e10adf42d8dd359e97fab65
Richard Calf <richard.calf@gmail.com>
2023-08-08 21:40:59 +0200

    section 10, video 120, adding the shopping list service

--- layered-cake/src/app/shopping-list/shopping-list.component.ts
+++ layered-cake/src/app/shopping-list/shopping-list.component.ts
@@ -1,20 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
+  providers: [ShoppingListService]
 })
-export class ShoppingListComponent {
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples',5),
-    new Ingredient('Tomatoes',10),
-  ];
+export class ShoppingListComponent implements OnInit {
+  constructor(private shoppingSerivce: ShoppingListService) {}
 
-  onShoppingAdded(item: Ingredient) {
-    console.log(item.name);
-    this.ingredients.push(item);
+  ingredients: Ingredient[];
+
+  ngOnInit() {
+    this.ingredients = this.shoppingSerivce.ingredients;
   }
 
+  onShoppingAdded(item: Ingredient) {
+    this.shoppingSerivce.addIngredient(item);
+  }
 }
--- layered-cake/src/app/shopping-list/shopping-list.service.ts
+++ layered-cake/src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,17 @@
+import { Ingredient } from "../shared/ingredient.model";
+
 export class ShoppingListService {
-    
+    ingredients: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
+        new Ingredient('Pears', 3)
+      ];
+
+      getIngredients() {
+        return this.ingredients.slice();
+      }
+
+      addIngredient(item: Ingredient) {
+        this.ingredients.push(item);
+      }
 }
