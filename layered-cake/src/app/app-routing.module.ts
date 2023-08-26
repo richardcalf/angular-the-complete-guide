f88f1a77ced024ae4fd07737a192db089abb8607
Richard Calf <richard.calf@gmail.com>
2023-08-26 20:59:56 +0200

    section 12, video 157, I've just added my own page not found page for fun

--- layered-cake/src/app/app-routing.module.ts
+++ layered-cake/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { RecipesComponent } from "./recipes/recipes.component";
 import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
     { path: 'recipes', component: RecipesComponent },
-    { path: 'shoppinglist', component: ShoppingListComponent }
+    { path: 'shoppinglist', component: ShoppingListComponent },
+    { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
--- layered-cake/src/app/app.module.ts
+++ layered-cake/src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-ed
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { AppRoutingModule } from './app-routing.module';
     RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DropdownDirective
+    DropdownDirective,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
