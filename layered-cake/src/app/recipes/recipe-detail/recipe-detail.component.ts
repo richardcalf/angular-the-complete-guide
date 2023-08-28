437a1da87f13ccaf1bff0ddd94a0e49769bd0205
Richard Calf <richard.calf@gmail.com>
2023-08-28 21:38:52 +0200

    section 12, video 167, programatic navigation to the edit page

--- layered-cake/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ layered-cake/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Params } from 'express-serve-static-core';
 
 @Component({
@@ -10,22 +10,32 @@ import { Params } from 'express-serve-static-core';
   styleUrls: ['./recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute) {}
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) {}
 
   recipe: Recipe;
+  index: number;
 
   ngOnInit() {
-    let recipeId = +this.route.snapshot.params['id'];
-    this.recipe = this.recipeService.getRecipe(recipeId);
+    // let recipeId = +this.route.snapshot.params['id'];
+    // this.recipe = this.recipeService.getRecipe(recipeId);
 
     this.route.params.subscribe(
       (p: Params) => {
-        this.recipe = this.recipeService.getRecipe(+p['id']);
+        this.index = +p['id'];
+        this.recipe = this.recipeService.getRecipe(this.index);
       }
-    )
+    );
   }
 
   onRecipeToShoppingList() {
     this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients);
+  }
+
+  onEditRecipe() {
+    //all three of these navigate methods do the same thing. to practice
+    // this.router.navigate(['edit'], {relativeTo: this.route});
+    // this.router.navigate(['recipes', this.index,  'edit']);
+    //or..
+    this.router.navigate(['../', this.index, 'edit' ], {relativeTo: this.route});
   }
 }
