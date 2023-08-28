fa92a88e72d39baf0843263cc49879dcb32e9719
Richard Calf <richard.calf@gmail.com>
2023-08-28 20:40:49 +0200

    section 12, video 166, retrieving route parameters

--- layered-cake/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ layered-cake/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
   templateUrl: './recipe-edit.component.html',
   styleUrls: ['./recipe-edit.component.css']
 })
-export class RecipeEditComponent {
+export class RecipeEditComponent implements OnInit {
+  id: number;
+  editMode = false;
+  
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    this.route.params.subscribe(
+      (p: Params) => {
+        this.id = +p['id'];
+        this.editMode = p['id'] != null;
+        console.log(this.editMode);
+      }
+    );
+  }
+  
 }
