66bb530c746b7f8dd87bbf047e615e6ca99f4373
Richard Calf <richard.calf@gmail.com>
2023-08-31 22:57:44 +0200

    section 13, video 178, subjects
    subjects as replacement of eventemitters
    
    only count if you use them as cross component
    
    event emitters
    
    where you manually call next
    
    using services
    
    don't use them when using @output()
    
    in that case use EventEmitter
    
    if you do subscribe to an emitter
    
    then it is a subject

--- understanding-observables-start/src/app/app.component.ts
+++ understanding-observables-start/src/app/app.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { UserService } from './user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   constructor(private userService: UserService) {}
 
   userActivated = false;
+  private activeSub: Subscription;
   
   ngOnInit() {
-    this.userService.activatedEmitter.subscribe(active => {
+    this.activeSub = this.userService.activatedEmitter.subscribe(active => {
       this.userActivated = active;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.activeSub.unsubscribe();
   }
 }
--- understanding-observables-start/src/app/user/user.component.ts
+++ understanding-observables-start/src/app/user/user.component.ts
@@ -20,6 +20,7 @@ export class UserComponent implements OnInit {
   }
 
   onActivate() {
-    this.userService.activatedEmitter.emit(true);
+    this.userService.activatedEmitter.next(true);
   }
+
 }
