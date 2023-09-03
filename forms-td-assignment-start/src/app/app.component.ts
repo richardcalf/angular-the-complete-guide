50535e5146a66c86dc9d60d4bf6f7a0fc4d7875d
Richard Calf <richard.calf@gmail.com>
2023-09-03 18:14:23 +0200

    assignment 6 : practicing template driven forms (complete)

--- forms-td-assignment-start/src/app/app.component.ts
+++ forms-td-assignment-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {  
+  @ViewChild('f') htmlForm: NgForm;
+  defaultTier = 'advanced';
+  submitted = false;
+
+  formInfo = {
+    email: '',
+    subscription: '',
+    password: ''
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.formInfo.email = this.htmlForm.value.formData.email;
+    this.formInfo.subscription = this.htmlForm.value.formData.subscription;
+    this.formInfo.password = this.htmlForm.value.formData.thepassword;
+    alert('thanks for your submission');
+  }
 }
