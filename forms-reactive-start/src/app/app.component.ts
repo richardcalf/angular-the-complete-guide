fe2010f454e4af2a9b23488e45e5fa43cd29ff34
Richard Calf <richard.calf@gmail.com>
2023-09-06 23:52:12 +0200

    section 15, video 212, reactive, creating custom validators

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -9,11 +9,12 @@ import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['male', 'female'];
   htmlForm: FormGroup;
+  nogoUserNames = ['Bonnie','Clyde'];
 
   ngOnInit(): void {
     this.htmlForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.nogoNames.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email])
     }),
       'gender': new FormControl('male'),
@@ -32,5 +33,12 @@ export class AppComponent implements OnInit {
   onAddHobby() {
     const control = new FormControl(null, Validators.required);
     (<FormArray>this.htmlForm.get('hobbies')).push(control);
+  }
+
+  nogoNames(control: FormControl): {[s: string]: boolean}  {
+    if (this.nogoUserNames.indexOf(control.value) !== -1 ) {
+      return {'nameNotAllowed': true};
+    }
+    return null;
   }
 }
