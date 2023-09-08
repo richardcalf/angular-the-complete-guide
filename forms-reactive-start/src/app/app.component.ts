753c4f10982d719e626d075148a60a894bbece19
Richard Calf <richard.calf@gmail.com>
2023-09-08 23:01:56 +0200

    section 15, video 214, reactive, creating custom async validators

--- forms-reactive-start/src/app/app.component.ts
+++ forms-reactive-start/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -15,7 +16,7 @@ export class AppComponent implements OnInit {
     this.htmlForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.nogoNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'email': new FormControl(null, [Validators.required, Validators.email], this.nogoEmails)
     }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
@@ -40,5 +41,18 @@ export class AppComponent implements OnInit {
       return {'nameNotAllowed': true};
     }
     return null;
+  }
+
+  nogoEmails(control: FormControl): Promise<any> | Observable<any>  {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailNotAllowed': true});
+        } else {
+          resolve(null);
+        }
+      }, 1200);
+    });
+    return promise;
   }
 }
