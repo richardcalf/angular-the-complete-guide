e74307eee57562bfc575a7ea36c09b3e6d21cde4
Richard Calf <richard.calf@gmail.com>
2023-09-10 22:14:03 +0200

    assignment 7 : practicing reactive forms: WIP:
    implement validator

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -1,9 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  projectStatus = ['Stable','Critical','Finished'];
+  htmlForm: FormGroup;
+  NoTestProjectName = 'Test';
+
+  ngOnInit() {
+    this.htmlForm = new FormGroup({
+      'projectname': new FormControl(null, [Validators.required, this.projectNameNotAllowed.bind(this)]),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'projectstatus': new FormControl(null, Validators.required)
+    });
+  }
+
+  onSubmit() {
+    console.log(this.htmlForm);
+  }
+
+  projectNameNotAllowed(control: FormControl): {[s: string]: boolean} {
+    if (this.NoTestProjectName === control.value) {
+    return {'testNameNotAllowed': true};
+    }
+    return null;
+  }
 }
