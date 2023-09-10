5a23f2a77ef1a860f66223bfc525a107cf944873
Richard Calf <richard.calf@gmail.com>
2023-09-10 22:59:00 +0200

    assignment 7 : practicing reactive forms (done)
    implement validator asynchronous

--- forms-reactive-assignment-start/src/app/app.component.ts
+++ forms-reactive-assignment-start/src/app/app.component.ts
@@ -11,23 +11,44 @@ export class AppComponent implements OnInit {
   projectStatus = ['Stable','Critical','Finished'];
   htmlForm: FormGroup;
   NoTestProjectName = 'Test';
+  statusUpdate: string = 'PENDING';
 
   ngOnInit() {
     this.htmlForm = new FormGroup({
-      'projectname': new FormControl(null, [Validators.required, this.projectNameNotAllowed.bind(this)]),
+      'projectname': new FormControl(null, [Validators.required], this.projectNameNotAllowedAsync),
       'email': new FormControl(null, [Validators.required, Validators.email]),
       'projectstatus': new FormControl(null, Validators.required)
     });
+
+    this.htmlForm.statusChanges.subscribe(
+      (s) => {
+        this.statusUpdate = s;
+        console.log(this.statusUpdate);
+      }
+    )
   }
 
   onSubmit() {
     console.log(this.htmlForm);
   }
 
-  projectNameNotAllowed(control: FormControl): {[s: string]: boolean} {
-    if (this.NoTestProjectName === control.value) {
-    return {'testNameNotAllowed': true};
-    }
-    return null;
+  // projectNameNotAllowed(control: FormControl): {[s: string]: boolean} {
+  //   if (this.NoTestProjectName === control.value) {
+  //   return {'testNameNotAllowed': true};
+  //   }
+  //   return null;
+  // }
+
+  projectNameNotAllowedAsync(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Test') {
+          resolve({'testNameNotAllowed': true});
+        } else {
+          resolve(null);
+        }
+      }, 1200);
+    });
+    return promise;
   }
 }
