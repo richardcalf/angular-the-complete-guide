1a4f65d7340c1936b39f25cb1339b35c8bf1857f
Richard Calf <richard.calf@gmail.com>
2023-09-22 21:56:48 +0200

    section 18, video 258, using rxjs operators to transform response data

--- http-01-start/src/app/app.component.ts
+++ http-01-start/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -13,7 +15,7 @@ export class AppComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.fetchPosts();
+    // this.fetchPosts();
   }
 
   onCreatePost(postData: { title: string; content: string }) {
@@ -34,6 +36,15 @@ export class AppComponent implements OnInit {
 
   private fetchPosts() {
     this.http.get(this.apiEndPoint)
+    .pipe(map(response => {
+      const posts = [];
+      for (const key in response) {
+        if(response.hasOwnProperty(key)) {
+          posts.push({ ...response[key], id: key});        
+        }
+      }
+      return posts;
+    }))
     .subscribe(posts => {
       console.log(posts);
     })
