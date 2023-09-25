f725d5dc66f398d9c17ff4c9fc0fea2dd1a87c43
Richard Calf <richard.calf@gmail.com>
2023-09-25 22:18:32 +0200

    section 19, video 282, storing recipes

--- layered-cake/src/app/shared/data-storage.service.ts
+++ layered-cake/src/app/shared/data-storage.service.ts
@@ -1,7 +1,17 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { RecipeService } from "../recipes/recipe.service";
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-    constructor(private https: HttpClient) {}
+    constructor(private https: HttpClient, private recipeService: RecipeService) {}
+    apiEndpoint = 'https://ng-layered-rc-default-rtdb.asia-southeast1.firebasedatabase.app/';
+
+    storeRecipes() {
+        const recipes = this.recipeService.getRecipes();
+        this.https.put(this.apiEndpoint+'recipes.json',recipes)
+        .subscribe(response => {
+            console.log(response);
+        })
+    }
 }
