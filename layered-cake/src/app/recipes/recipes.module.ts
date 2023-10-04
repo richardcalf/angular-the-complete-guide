bd51e23b49ff3f2e8b13a48ab9039d90c1f1060d
Richard Calf <richard.calf@gmail.com>
2023-10-04 21:47:07 +0200

    section 22, video 325, splitting modules correctly

--- layered-cake/src/app/recipes/recipes.module.ts
+++ layered-cake/src/app/recipes/recipes.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { CommonModule } from "@angular/common";
+
 import { RecipesComponent } from "./recipes.component";
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
 import { RecipeNotSelectedComponent } from "./recipe-not-selected/recipe-not-selected.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { ReactiveFormsModule } from "@angular/forms";
+
 
 @NgModule({
     declarations: [
@@ -15,6 +20,7 @@ import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
         RecipeNotSelectedComponent,
         RecipeEditComponent,
     ],
+    imports: [RouterModule, CommonModule, ReactiveFormsModule],
     exports: [
         RecipesComponent,
         RecipeListComponent,
