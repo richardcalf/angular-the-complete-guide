bf460e25030f6d9f8b59e017ef38345d9e495ef7
Richard Calf <richard.calf@gmail.com>
2023-10-05 22:00:06 +0200

    section 22, video 335, preloading lazy loaded code

--- layered-cake/src/app/app-routing.module.ts
+++ layered-cake/src/app/app-routing.module.ts
@@ -1,20 +1,18 @@
 import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
-import { AuthGaurd } from "./auth/auth.gaurd";
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' },
     { path: 'recipes', loadChildren: () => import('./recipes/recipes.module').then(m => m.RecipesModule) },
     { path: 'shoppinglist', loadChildren: () => import('./shopping-list/shopping-list.module').then(s => s.ShoppingListModule) },
     { path: 'auth', loadChildren: () => import('./auth/auth.module').then(a => a.AuthModule) },
-    
     { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
     imports: [
-        RouterModule.forRoot(appRoutes)
+        RouterModule.forRoot(appRoutes, { preloadingStrategy: PreloadAllModules })
     ],
     exports: [RouterModule]
 })
