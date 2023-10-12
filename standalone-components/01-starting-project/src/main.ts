8bb45f11b561185a98c974953f8509ae0ddbb4d0
Richard Calf <richard.calf@gmail.com>
2023-10-12 13:08:40 +0200

    section 24, video 352, services and standalone components
    service providers array global in application

--- standalone-components/01-starting-project/src/main.ts
+++ standalone-components/01-starting-project/src/main.ts
@@ -3,10 +3,13 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
 import { environment } from './environments/environment';
+import { AnalyticsService } from './app/shared/analytics.service';
 
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent, {
+  providers: [AnalyticsService]
+});
