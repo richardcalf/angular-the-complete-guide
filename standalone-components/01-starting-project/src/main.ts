3126393ed1ccd3be8f88afc6a0c224104b4dc42a
Richard Calf <richard.calf@gmail.com>
2023-10-12 13:37:14 +0200

    section 24, video 353, routing with standalone components
    mix of standalone and ngmodules

--- standalone-components/01-starting-project/src/main.ts
+++ standalone-components/01-starting-project/src/main.ts
@@ -1,13 +1,17 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, importProvidersFrom } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 
 import { environment } from './environments/environment';
-import { AnalyticsService } from './app/shared/analytics.service';
+import { AppRoutingModule } from './app/app-routing.module';
 
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrapApplication(AppComponent);
+bootstrapApplication(AppComponent,{
+  providers: [
+    importProvidersFrom(AppRoutingModule)
+  ]
+});
