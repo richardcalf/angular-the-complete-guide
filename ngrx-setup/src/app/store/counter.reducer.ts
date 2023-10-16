2a0c2863502390f5fdaa7cadcb739b61457b7f29
Richard Calf <richard.calf@gmail.com>
2023-10-16 15:48:21 +0200

    section 26, video 377, an alternate way of defining actions

--- ngrx-setup/src/app/store/counter.reducer.ts
+++ ngrx-setup/src/app/store/counter.reducer.ts
@@ -1,5 +1,7 @@
-import { createReducer, on } from "@ngrx/store";
-import { increment } from "./counter.actions";
+import { Action, createReducer, on } from "@ngrx/store";
+import { CounterActions, INCREMENT, IncrementAction } from "./counter.actions";
+
+// import { increment } from './counter.actions';
 
 const initialState = 0;
 
@@ -8,9 +10,10 @@ const initialState = 0;
 //     on(increment, (state, action) => state + action.value)
 // );
 
-export function counterReducer(state = initialState, action: any) {
-    if(action.type === '[Counter] Increment') {
-        return state + action.value;
+export function counterReducer(state = initialState, action: CounterActions | Action) {
+    if(action.type === INCREMENT) {
+        return state + (action as IncrementAction).value;
     }
     return state;
 }
+
