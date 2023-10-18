a1bae40ed6f11914dd836b73af4abd45da5843a9
Richard Calf <richard.calf@gmail.com>
2023-10-18 11:13:19 +0200

    section 26, video 378, practice a second action (solved)

--- ngrx-setup/src/app/counter-controls/counter-controls.component.ts
+++ ngrx-setup/src/app/counter-controls/counter-controls.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { increment } from '../store/counter.actions';
+import { increment, decrement } from '../store/counter.actions';
 // import { IncrementAction } from '../store/counter.actions';
 
 
@@ -17,6 +17,6 @@ export class CounterControlsComponent {
   }
 
   decrement() {
-    
+    this.store.dispatch(decrement({ value: 2 }))    
   }
 }
--- ngrx-setup/src/app/store/counter.reducer.ts
+++ ngrx-setup/src/app/store/counter.reducer.ts
@@ -1,14 +1,16 @@
 import { Action, createReducer, on } from "@ngrx/store";
 // import { CounterActions, INCREMENT, IncrementAction } from "./counter.actions";
 
-import { increment } from './counter.actions';
+import { decrement, increment } from './counter.actions';
 
 const initialState = 0;
 
 export const counterReducer = createReducer(
     initialState,
-    on(increment, (state, action) => state + action.value)
+    on(increment, (state, action) => state + action.value),
+    on(decrement, (state, action) => state - action.value)
 );
+
 
 // export function counterReducer(state = initialState, action: CounterActions | Action) {
 //     if(action.type === INCREMENT) {
